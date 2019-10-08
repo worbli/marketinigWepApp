52e9172b532aa4a7987196a61c8438397a57bfc6
bkawk <email.will.in.china@gmail.com>
2019-10-08 20:11:43 +0800

    Updates

---
+++ precache-manifest.14ef4017205d5c109a68236d2d3dd728.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc92d171ba5322b0ae35ae175f39f1cd",
+    "url": "/marketinigWepApp/index.html"
+  },
+  {
+    "revision": "c0edf560562136bd4c7a",
+    "url": "/marketinigWepApp/static/css/main.ef098f80.chunk.css"
+  },
+  {
+    "revision": "ab76fb5f8363d8717384",
+    "url": "/marketinigWepApp/static/js/2.5251ecd7.chunk.js"
+  },
+  {
+    "revision": "c0edf560562136bd4c7a",
+    "url": "/marketinigWepApp/static/js/main.ba9a935d.chunk.js"
+  },
+  {
+    "revision": "a800cf6861802d30e608",
+    "url": "/marketinigWepApp/static/js/runtime-main.eeec4441.js"
+  },
+  {
+    "revision": "7b21f96e762e3d959c1980ef08283b69",
+    "url": "/marketinigWepApp/static/media/blue-chevron.7b21f96e.svg"
+  },
+  {
+    "revision": "19f1c5e1a3317be31004e282d8ea8807",
+    "url": "/marketinigWepApp/static/media/image1.19f1c5e1.jpg"
+  },
+  {
+    "revision": "a6136ec52012965a2f8da893dc7b8d71",
+    "url": "/marketinigWepApp/static/media/left-dark.a6136ec5.svg"
+  },
+  {
+    "revision": "1b991c132f26c2dbc663f871bf816363",
+    "url": "/marketinigWepApp/static/media/left-light.1b991c13.svg"
+  },
+  {
+    "revision": "b4a9d276db512a32ec2e8ee51fc1456c",
+    "url": "/marketinigWepApp/static/media/logo-dark.b4a9d276.svg"
+  },
+  {
+    "revision": "683c1c36e846449045db9b40a88d8377",
+    "url": "/marketinigWepApp/static/media/logo-light.683c1c36.svg"
+  },
+  {
+    "revision": "194954724d1513c46d74ff3915c678ed",
+    "url": "/marketinigWepApp/static/media/right-dark.19495472.svg"
+  },
+  {
+    "revision": "f009f6f0dc38866377f27c775e64f0dd",
+    "url": "/marketinigWepApp/static/media/right-light.f009f6f0.svg"
+  }
+]);
