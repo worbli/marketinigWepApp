7004a947d271528429d363d1f116084c2b75828d
mitchmahaf <57337193+mitchmahaf@users.noreply.github.com>
2019-11-12 14:40:55 +1000

    Added text

--- src/localization/pages/home.tsx
+++ src/localization/pages/home.tsx
@@ -35,9 +35,9 @@ const HomeCopy: { [key: string]: TranslationsInterface; } = {
     joinCardOneIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
     joinCardTwoIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
     joinCardThreeIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
-    progressCardOneIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardTwoIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardThreeIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
+    progressCardOneIntro: 'Browse WORBLI related articles and news',
+    progressCardTwoIntro: 'Keep up to date with the new and exciting applications launching on WORBLI',
+    progressCardThreeIntro: 'View our proposed and past technical updates and current chain specifications',
   },
   kr: {
     language: '한국어',
@@ -54,9 +54,9 @@ const HomeCopy: { [key: string]: TranslationsInterface; } = {
     joinCardOneIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
     joinCardTwoIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
     joinCardThreeIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet sit amet tempor mollis, sollicitudin nec velit. Nunc condimentum tempus felis, vel tincidunt libero viverra vitae. In pellentesque eget metus sit amet tincidunt. Nullam sollicitudin urna quis dolor convallis ullamcorper. Pellentesque at sodales lectus. Nullam dolor velit, accumsan consequat ipsum in, blandit elementum massa.',
-    progressCardOneIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardTwoIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardThreeIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
+    progressCardOneIntro: 'Browse WORBLI related articles and news',
+    progressCardTwoIntro: 'Keep up to date with the new and exciting applications launching on WORBLI',
+    progressCardThreeIntro: 'View our proposed and past technical updates and current chain specifications',
   },
   en: {
     language: 'English',
@@ -73,9 +73,9 @@ const HomeCopy: { [key: string]: TranslationsInterface; } = {
     joinCardOneIntro: 'Are you working on a blockchain application that needs to be compliant with global laws and regulations? If so, WORBLI may be the right choice for you.',
     joinCardTwoIntro: 'The WORBLI ecosystem is about cooperation. If you can provide a core service to the network, such as identity verification, dispute resolution or developer integrations, we’d love to hear from you.',
     joinCardThreeIntro: 'Join the financial revolution by creating your WORBLI account. Get verified once and gain access to all WORBLI-powered financial services.  With an identity based recoverable account on WORBLI you can interact with the newest DeFi applications with a single authorization.  The account application process is quick, easy and free.',
-    progressCardOneIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardTwoIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
-    progressCardThreeIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam massa, laoreet',
+    progressCardOneIntro: 'Browse WORBLI related articles and news',
+    progressCardTwoIntro: 'Keep up to date with the new and exciting applications launching on WORBLI',
+    progressCardThreeIntro: 'View our proposed and past technical updates and current chain specifications',
   },
 }
 export { HomeCopy }
